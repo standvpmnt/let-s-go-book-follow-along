4ff13153e44dbae6ccc784f97f1e41abe588ac70
standvpmnt <standvpmnt@gmail.com>
2022-12-14 22:13:04 +0530

    Add middlewares for logging requests, recovering from panic and for add security headers to response

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ checksums as stored in go.sum file.
 `go get -u <package>` to upgrade to a specific version
 
 To cleanup run, `go mod tidy -v` or `go get <package>@none`
+
+For chaining middlewares, look at [package](github.com/justinas/alice@v1)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // the routes() method returns a servemux containing our application routes
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +17,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
