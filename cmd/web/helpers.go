174d616116d137c767c96b61e0db4fb511754113
standvpmnt <standvpmnt@gmail.com>
2022-12-18 07:50:51 +0530

    Implement login and signup for a user along with using csrf protection with cookies

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -49,8 +50,10 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 func (app *application) decodePostForm(r *http.Request, dst any) error {
@@ -71,4 +74,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -6,17 +6,16 @@ import (
 	"time"
 )
 
-
 type Snippet struct {
-	ID	int
-	Title	string
-	Content		string
-	Created		time.Time
-	Expires		time.Time
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
 }
 
 type SnippetModel struct {
-	DB	*sql.DB
+	DB *sql.DB
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
@@ -57,9 +56,9 @@ func (m *SnippetModel) Latest() ([]*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 		WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
 	rows, err := m.DB.Query(stmt)
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 	defer rows.Close()
 
 	snippets := []*Snippet{}
